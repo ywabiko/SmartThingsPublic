a01824d8bf0f6752f5ae9b881ae9f66575179a66
Yasuhiro Wabiko <ywabiko@user.noreply.github.com>
2015-12-15 00:25:41 -0800

    untabify, cosmetic fix

--- smartapps/ywabiko/garage-light-by-motion.src/garage-light-by-motion.groovy
+++ smartapps/ywabiko/garage-light-by-motion.src/garage-light-by-motion.groovy
@@ -15,18 +15,18 @@
  */
 definition(
     name: "Garage Light By Motion",
-    namespace: "ywabiko",
-    author: "Yasuhiro Wabiko",
-    description: "Turn on light when any of motion sensors reports motion.\r\nTurn off light when none of motion sensors report motion.\r\n",
-    category: "My Apps",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    oauth: true)
+           namespace: "ywabiko",
+           author: "Yasuhiro Wabiko",
+           description: "Turn on light when any of motion sensors reports motion.\r\nTurn off light when none of motion sensors report motion.\r\n",
+           category: "My Apps",
+           iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+           iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+           iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+           oauth: true)
 
 preferences {
-	section("Motion Sensors") {
-		input "themotions", "capability.motionSensor", required: true, title:"Where?", multiple:true
+    section("Motion Sensors") {
+        input "themotions", "capability.motionSensor", required: true, title:"Where?", multiple:true
     }
     section("Light Switches") {
         input "theswitches", "capability.switch", required: true, title:"Where?", multiple:true
@@ -34,46 +34,46 @@ preferences {
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
-	initialize()
+    log.debug "Installed with settings: ${settings}"
+    initialize()
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
-	unsubscribe()
-	initialize()
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
 }
 
 def initialize() {
-	for (themotion in themotions) {
-		subscribe(themotion, "motion.active",   motionDetectedHandler)
-		subscribe(themotion, "motion.inactive", motionStoppedHandler)
-	}
+    for (themotion in themotions) {
+        subscribe(themotion, "motion.active",   motionDetectedHandler)
+        subscribe(themotion, "motion.inactive", motionStoppedHandler)
+    }
 }
 
 def motionDetectedHandler(evt) {
-	if (themotions.any { it.value == "active" })
+    if (themotions.any { it.value == "active" })
     {
-		for (theswitch in theswitches)
-		{
-        	theswitch.on();
-		}
+        for (theswitch in theswitches)
+        {
+            theswitch.on();
+        }
         // leave the lights on for another 10 min.
         // This timeout will be extended as long as any sensor keeps reporting motion.
     	runIn(600, scheduledHandler, [overwrite: true])
-	}
+    }
 }
 
 def motionStoppedHandler(evt) {
-	if (themotions.every { it.value == "inactive" })
+    if (themotions.every { it.value == "inactive" })
     {
-		runIn(600, scheduledHandler, [overwrite: true])
-	}
+        runIn(600, scheduledHandler, [overwrite: true])
+    }
 }
 
 def scheduledHandler(evt) {
-	for (theswitch in theswitches)
-	{
+    for (theswitch in theswitches)
+    {
        	theswitch.off();
-	}
+    }
 }
